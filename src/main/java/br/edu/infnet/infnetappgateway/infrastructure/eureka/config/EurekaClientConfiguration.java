8686f2431a13272c4c39bf942d060d4f6d686aed
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-11-30 22:16:45 -0300

    First commit

---
+++ src/main/java/br/edu/infnet/infnetappgateway/infrastructure/eureka/config/EurekaClientConfiguration.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.infnetappgateway.infrastructure.eureka.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaClient
+public class EurekaClientConfiguration {
+}
